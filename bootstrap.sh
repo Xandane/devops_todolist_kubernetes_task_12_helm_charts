c6638c4ba6888297e341c4835d1cbdc40eff7355
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-05-05 11:29:32 +0300

    add helm

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,9 @@
 kubectl create namespace todoapp-ns --dry-run=client -o yaml | kubectl apply -f -
 # Deploy helm chart
 helm install todoapp ./helm-chart/todoapp --namespace todoapp-ns --create-namespace
+# Створення кластера
+kind create cluster --name mycluster --config cluster.yml
+# Перевірка нод
+kubectl get nodes --show-labels
+# Тейнінг нод з app=mysql
+kubectl taint nodes -l app=mysql app=mysql:NoSchedule
